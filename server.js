726c3e76476f2918a5232a10b38754673f8e2172
Phazeon <tjl82996@gamil.com>
2018-06-25 22:40:31 -0700

    ALMOST DONE

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+app.use(express.static(__dirname + "../public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(methodOverride("__method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+
+  console.log("Server listening on: http://localhost:" + PORT);
+});
